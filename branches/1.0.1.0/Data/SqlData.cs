5286ed30dbee181c9c78ffb9d2f53f733816dea6
fred.sobel <fred.sobel@61609516-7fa2-11dd-ac5a-f353d88c4ef8>
2009-09-14 03:47:46 +0000

    adding oracle and refactoring

---
+++ branches/1.0.1.0/Data/SqlData.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Test.SqlCopy.Data
+{
+    public class SqlData
+    {
+        public IDataReader SelectTable(string db, string table)
+        {
+            SqlConnection source = new SqlConnection(db);
+
+            string sql = string.Format("select * from {0}", table);
+
+            SqlCommand command = new SqlCommand(sql, source);
+
+            source.Open();
+            IDataReader dr = command.ExecuteReader(CommandBehavior.CloseConnection);
+
+            return dr;
+        }
+
+        public void DeleteTable(string db, string table)
+        {
+            using (SqlConnection destination = new SqlConnection(db))
+            {
+                string sql = string.Format("delete from {0};", table);
+
+                SqlCommand command = new SqlCommand(sql, destination);
+
+                destination.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+
+        public IDataReader GetTables(string db)
+        {
+            string sql = @"SELECT '[' + table_schema + '].[' + table_name + ']' as table_name FROM information_schema.tables";
+
+            SqlConnection source = new SqlConnection(db);
+            SqlCommand command = new SqlCommand(sql, source);
+            source.Open();
+            IDataReader dr = command.ExecuteReader(CommandBehavior.CloseConnection);
+
+            return dr;
+        }
+
+
+        //Disable Constraints for all tables
+        //exec sp_msforeachtable "ALTER TABLE ? NOCHECK CONSTRAINT all"
+        //exec sp_msforeachtable "ALTER TABLE ? DISABLE TRIGGER all"
+        public void PreCopySql(string db)
+        {
+            using (SqlConnection destination = new SqlConnection(db))
+            {
+                //string sql = "exec sp_msforeachtable 'ALTER TABLE ? NOCHECK CONSTRAINT all'; exec sp_msforeachtable 'ALTER TABLE ? DISABLE TRIGGER all'; ";
+                string sql = Properties.Settings.Default.PreCopySql;
+
+                SqlCommand command = new SqlCommand(sql, destination);
+
+                destination.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+        //Turn constraints and triggers back on
+        //exec sp_msforeachtable @command1="print '?'", @command2="ALTER TABLE ? CHECK CONSTRAINT all"
+        //exec sp_msforeachtable @command1="print '?'", @command2="ALTER TABLE ? ENABLE TRIGGER all"
+        public void PostCopySql(string db)
+        {
+            using (SqlConnection destination = new SqlConnection(db))
+            {
+                //string sql = "exec sp_msforeachtable 'ALTER TABLE ? CHECK CONSTRAINT all'; exec sp_msforeachtable 'ALTER TABLE ? ENABLE TRIGGER all'; ";
+                string sql = Properties.Settings.Default.PostCopySql;
+
+                SqlCommand command = new SqlCommand(sql, destination);
+
+                destination.Open();
+                command.ExecuteNonQuery();
+            }
+        }
+
+
+    }
+}
---
+++ branches/1.0.1.0/Objects/CopyObject.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test.SqlCopy.Objects
+{
+    [Serializable()]
+    public class CopyObject
+    {
+        public string Source { get; set; }
+        public string Destination { get; set; }
+        public int BatchSize { get; set;}
+        public int BulkCopyOptions { get; set; }
+        public int BulkCopyTimeout { get; set; }
+        public int NotifyAfter { get; set; }
+        public string DestinationTableName { get; set; }
+        public bool Default { get; set; }
+        public bool UseInternalTransaction { get; set; }  
+
+        // Oracle only        
+        public string DestinationPartitionName { get; set; }        
+
+        // Sql server only
+        public bool CheckConstraints { get; set; }        
+        public bool FireTriggers { get; set; }
+        public bool KeepIdentity { get; set; }
+        public bool KeepNulls { get; set; }
+        public bool TableLock { get; set; }
+
+        // Custom
+        public bool DeleteRows { get; set; }
+        public string PreCopySql { get; set; }
+        public string PostCopySql { get; set; }
+
+        //@"SELECT '[' + table_schema + '].[' + table_name + ']' as table_name FROM information_schema.tables";
+        public string GetTableSql { get; set; }
+
+        //delete from {0};"
+        public string DeleteSql { get; set; }
+    }
+}
